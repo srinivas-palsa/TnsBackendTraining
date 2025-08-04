9782a07c87c5ad8c8746a9b3f3f72702f977ae5f
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-04 21:33:20 +0530

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	void print(){
+		System.out.print("Welcome to TNS Training");
+	}
+
+	public static void main(String[] args) {
+		Hello obj = new Hello();
+		obj.print();
+	}
+
+}
