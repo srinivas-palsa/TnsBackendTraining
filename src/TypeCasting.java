aa2c650bf2c7a2d328f65afb9142568f5dc4cf28
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-05 23:20:01 +0530

    Type Casting Example

---
+++ src/TypeCasting.java
@@ -0,0 +1,21 @@
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		// Implicit type casting
+		
+		short x = 80;
+		int y = x;
+		System.out.println(y);	
+		
+		
+		// Explicit type casting
+		
+		int m = 10;
+		short n = (short)m;
+		System.out.println(n);
+
+	}
+
+}
