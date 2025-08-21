8a948c255444e0352792e16b900b6367d9499781
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-21 21:35:19 +0530

    Added comment

--- src/WithoutException.java
+++ src/WithoutException.java
@@ -1,11 +1,12 @@
+// Demo for exception handling without using exception
 
 public interface WithoutException {
 	
 	public static void main(String[] args) {
 		
-		int x = 9/0;
+		int x = 9/0; // Any number can not be divisible by Zero	
 		
-		System.out.println("The result is :");
+		System.out.println("The result is :"+x);
 	}
 
 }
