805919f1ffabe8e4f06efbbdccb525d2119a25f6
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-21 21:39:38 +0530

    Used try, catch blocks

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -3,6 +3,20 @@ public class ExceptionHandling {
 
 	public static void main(String[] args) {
 		
+		
+		try {
+			
+			int x = 19/0;
+			System.out.println("The result is:"+x);
+		}
+		
+		catch(ArithmeticException e) {
+			
+			System.err.println("You cannot divide a number by Zero"
+					+ "Please change the value");
+			
+		}
+		
 
 	}
 
