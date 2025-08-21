f1077f51aeb69eeb42aeaa85042e50b89aad8595
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-21 22:43:07 +0530

    One catch to rule them all

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -27,7 +27,7 @@ public class ExceptionHandling {
 */
 
 
-
+/*
 // Using multiple catch blocks
 
 public class ExceptionHandling{
@@ -56,6 +56,34 @@ public class ExceptionHandling{
 			System.err.println("The defined index is not valid"
 					+ "Please enter a valid index");
 			
+		}
+		
+		
+}
+}
+
+*/
+
+
+
+public class ExceptionHandling{
+	
+	public static void main(String args[]) {
+		
+		try {
+			
+			int x = 7/5;
+			System.out.println("The result is: "+x);
+			
+			int arr[]= new int[5];
+			arr[6] = 12;
+			System.out.println("The resultant array is: "+arr[4]);
+			
+		}
+		
+		catch(ArithmeticException|ArrayIndexOutOfBoundsException ae) {
+			
+			System.err.println(ae);
 		}
 		
 		
