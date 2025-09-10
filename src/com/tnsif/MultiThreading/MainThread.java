28422a1d552ac36b2db4802ec0e4650ef1b45209
srinivas-palsa <palsasrinivas03@gmail.com>
2025-09-10 19:01:28 +0530

    MultiThreading Example

---
+++ src/com/tnsif/MultiThreading/MainThread.java
@@ -0,0 +1,25 @@
+package com.tnsif.MultiThreading;
+
+public class MainThread {
+
+	public static void main(String args[]){
+		
+		UsingThreadClass obj = new UsingThreadClass();
+		
+		Thread t1 = new Thread() {
+			public void run() {
+				obj.multiplication(5);
+				
+			}
+		};
+		
+		Thread t2 = new Thread() {
+			public void run() {
+				obj.multiplication(3);
+			}
+		};
+		t1.start();
+		t2.start();
+		
+	}
+}
---
+++ src/com/tnsif/MultiThreading/UsingThreadClass.java
@@ -0,0 +1,14 @@
+package com.tnsif.MultiThreading;
+
+// Demo for multi threading 
+public class UsingThreadClass extends Thread{
+	
+	public synchronized void multiplication(int num) {
+		
+		for(int i=0; i<=5; i++) {
+		
+		System.out.println(num*i);
+		}
+	}
+
+}
