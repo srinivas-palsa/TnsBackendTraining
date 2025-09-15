da852d298888bceddc495b43b5d317ca35d50d01
srinivas-palsa <palsasrinivas03@gmail.com>
2025-09-15 23:38:34 +0530

    Type Casting Example

---
+++ src/com/tnsif/TypeCastingExample/TypeCasting.java
@@ -0,0 +1,22 @@
+package com.tnsif.TypeCastingExample;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		// Implicit type casting
+		
+		short x = 80;
+		int y = x;
+		System.out.println(y);	
+		
+		
+		// Explicit type casting
+		
+		int m = 10;
+		short n = (short)m;
+		System.out.println(n);
+
+	}
+
+}
